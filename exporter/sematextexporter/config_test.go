8ed1a5aa2c03b4b355cda60c9a515000c0bedcb4
Akhigbe Eromosele <akhigbeeromo@gmail.com>
2024-10-04 18:44:48 +0100

    Rewrite test for config

--- exporter/sematextexporter/config_test.go
+++ exporter/sematextexporter/config_test.go
@@ -56,6 +56,7 @@ func TestLoadConfig(t *testing.T) {
 					RandomizationFactor: backoff.DefaultRandomizationFactor,
 					Multiplier:          backoff.DefaultMultiplier,
 				},
+				Region: "US",
 				App_token:       "my-token",
 				MetricsSchema:   "otel-v1",
 				PayloadMaxLines: 72,
